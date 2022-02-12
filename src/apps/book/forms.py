9fc5c7b59ed12e79c26e2eadf020ff24db9113d7
M Afif Nur Azizi <afifnurazizi@gmail.com>
2022-02-13 01:46:31 +0800

    CU-27jd88e - As Admin, I will be able to add contributor

---
+++ src/apps/book/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class AddContributorForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=50, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Name'
+    }))
+    description = forms.CharField(label='Description', max_length=50, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Description'
+    }))
--- src/apps/book/urls.py
+++ src/apps/book/urls.py
@@ -3,4 +3,5 @@ from .views import ContributorListView
 
 urlpatterns = [
     path('contributor/', ContributorListView.as_view(), name='contributor_list'),
+    path('contributor/save', ContributorListView.as_view(), name='contributor_save'),
 ]
