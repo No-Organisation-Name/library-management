0f6bc3f073c4ced382e6ca17665c204ccd46a3a6
M Afif Nur Azizi <afifnurazizi@gmail.com>
2022-03-16 19:27:03 +0800

    CU-2d9jdjy - As Admin, I will be able to add type of membership

--- src/apps/membership/views.py
+++ src/apps/membership/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#import reverse 
+from django.urls import reverse
 from .models import Type
 from django.views import View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from .forms import AddTypeOfMemberForm
 class ListTypeView(View):
     template_name = 'list_type.html'
 
@@ -10,6 +12,7 @@ class ListTypeView(View):
         obj = Type.objects.all()
         paginator = Paginator(obj, 5)
         page = request.GET.get('page')
+        form = AddTypeOfMemberForm()
         try:
             type_of_members = paginator.page(page)
         except PageNotAnInteger:
@@ -21,4 +24,17 @@ class ListTypeView(View):
             'type_of_member':type_of_members,
             'range':paginator.page_range,
             'page_now':type_of_members.number,
+            'form':form,
         })
+
+    def post(self, request):
+        form = AddTypeOfMemberForm(request.POST)
+        if form.is_valid():
+            type_of_member = Type()
+            type_of_member.name = form.cleaned_data['name']
+            type_of_member.span_of_time = form.cleaned_data['span_of_time']
+            type_of_member.fine = form.cleaned_data['fine']
+            type_of_member.amount_of_book = form.cleaned_data['amount_of_book']
+            type_of_member.cost = form.cleaned_data['cost']
+            type_of_member.save()
+        return redirect(reverse('type_list'))
