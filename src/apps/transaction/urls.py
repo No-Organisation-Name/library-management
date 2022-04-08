9a902ae9c213d0cde7eb35ba825671cc179488b4
M Afif Nur Azizi <afifnurazizi@gmail.com>
2022-04-08 15:45:25 +0800

    CU-2d9jdk3 - As Admin, I will be able to view the loan history of user and membership

--- src/apps/transaction/urls.py
+++ src/apps/transaction/urls.py
@@ -3,4 +3,6 @@ from .views import *
 urlpatterns = [
     path('search', SearchView.as_view(), name='transaction_search'),
     path('search/result', SearchUserView.as_view(), name='transaction_search_result'), 
+    path('search/result/new', CreateUserView.as_view(), name='add_new_user'), 
+    path('<int:id>/user', DetailUserView.as_view(), name='detail_user')
 ]
